d404725fda0c47e5d69761fcf38d02d22c7c54c9
qkrdmsql <kelly030813@kaist.ac.kr>
2024-01-05 14:32:25 +0900

    default

---
+++ app/src/main/java/com/example/madcamp_week2_kjy_peb/Login.kt
@@ -0,0 +1,11 @@
+package com.example.madcamp_week2_kjy_peb
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Login : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
