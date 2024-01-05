8480bc82c098c52879526dc2ee8d696542701201
jooyoung9939 <geore0309@korea.ac.kr>
2024-01-05 14:33:40 +0900

    회원가입 xml 만듬

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.example.madcamp_week2_kjy_peb"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.madcamp_week2_kjy_peb"
